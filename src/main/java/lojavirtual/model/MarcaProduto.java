a897aaff44e7dd0292b99b9719d868c982c88027
luangf <luangf222@gmail.com>
2023-04-21 23:46:05 -0300

    commit classe categoria produto

--- src/main/java/lojavirtual/model/MarcaProduto.java
+++ src/main/java/lojavirtual/model/MarcaProduto.java
@@ -2,6 +2,7 @@ package lojavirtual.model;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -20,6 +21,7 @@ public class MarcaProduto implements Serializable {
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_marca_produto")
 	private Long id;
 
+	@Column(nullable = false)
 	private String nomeDesc;
 
 	public Long getId() {
