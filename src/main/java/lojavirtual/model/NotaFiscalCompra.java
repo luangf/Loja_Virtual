23bc686e9c1e638d8821cfe354996739ae5496d2
luangf <luangf222@gmail.com>
2023-04-22 13:31:06 -0300

    22-04-2023 13:30

--- src/main/java/lojavirtual/model/NotaFiscalCompra.java
+++ src/main/java/lojavirtual/model/NotaFiscalCompra.java
@@ -6,8 +6,6 @@ import java.util.Date;
 
 import javax.persistence.ConstraintMode;
 import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
 import javax.persistence.ForeignKey;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -19,57 +17,34 @@ import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
-import lojavirtual.enums.StatusContaPagar;
-
 @Entity
-@Table(name = "conta_pagar")
-@SequenceGenerator(name = "seq_conta_pagar", sequenceName = "seq_conta_pagar", allocationSize = 1, initialValue = 1)
-public class ContaPagar implements Serializable {
+@Table(name = "nota_fiscal_compra")
+@SequenceGenerator(name = "seq_nota_fiscal_compra", sequenceName = "seq_nota_fiscal_compra", allocationSize = 1, initialValue = 1)
+public class NotaFiscalCompra implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_conta_pagar")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_nota_fiscal_compra")
 	private Long id;
 
-	private String descricao;
-
-	@Enumerated(EnumType.STRING)
-	private StatusContaPagar status;
-
+	private String numeroNota;
+	private String serieNota;
+	private String descricaoObs;
 	private BigDecimal valorTotal;
 	private BigDecimal valorDesconto;
+	private BigDecimal valorIcms;
 
 	@Temporal(TemporalType.DATE)
-	private Date dtVencimento;
-
-	@Temporal(TemporalType.DATE)
-	private Date dtPagamento;
+	private Date dataCompra;
 
 	@ManyToOne(targetEntity = Pessoa.class)
 	@JoinColumn(name = "pessoa_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "pessoa_fk"))
 	private Pessoa pessoa;
 
-	@ManyToOne(targetEntity = Pessoa.class)
-	@JoinColumn(name = "pessoa_forn_id", nullable = false,
-		foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "pessoa_forn_fk"))
-	private Pessoa pessoa_fornecedor;
-
-	public StatusContaPagar getStatus() {
-		return status;
-	}
-
-	public void setStatus(StatusContaPagar status) {
-		this.status = status;
-	}
-
-	public Pessoa getPessoa_fornecedor() {
-		return pessoa_fornecedor;
-	}
-
-	public void setPessoa_fornecedor(Pessoa pessoa_fornecedor) {
-		this.pessoa_fornecedor = pessoa_fornecedor;
-	}
+	@ManyToOne
+	@JoinColumn(name = "conta_pagar_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "conta_pagar_id_fk"))
+	private ContaPagar contaPagar;
 
 	public Long getId() {
 		return id;
@@ -79,28 +54,28 @@ public class ContaPagar implements Serializable {
 		this.id = id;
 	}
 
-	public String getDescricao() {
-		return descricao;
+	public String getNumeroNota() {
+		return numeroNota;
 	}
 
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
+	public void setNumeroNota(String numeroNota) {
+		this.numeroNota = numeroNota;
 	}
 
-	public Date getDtVencimento() {
-		return dtVencimento;
+	public String getSerieNota() {
+		return serieNota;
 	}
 
-	public void setDtVencimento(Date dtVencimento) {
-		this.dtVencimento = dtVencimento;
+	public void setSerieNota(String serieNota) {
+		this.serieNota = serieNota;
 	}
 
-	public Date getDtPagamento() {
-		return dtPagamento;
+	public String getDescricaoObs() {
+		return descricaoObs;
 	}
 
-	public void setDtPagamento(Date dtPagamento) {
-		this.dtPagamento = dtPagamento;
+	public void setDescricaoObs(String descricaoObs) {
+		this.descricaoObs = descricaoObs;
 	}
 
 	public BigDecimal getValorTotal() {
@@ -119,12 +94,36 @@ public class ContaPagar implements Serializable {
 		this.valorDesconto = valorDesconto;
 	}
 
+	public BigDecimal getValorIcms() {
+		return valorIcms;
+	}
+
+	public void setValorIcms(BigDecimal valorIcms) {
+		this.valorIcms = valorIcms;
+	}
+
+	public Date getDataCompra() {
+		return dataCompra;
+	}
+
+	public void setDataCompra(Date dataCompra) {
+		this.dataCompra = dataCompra;
+	}
+
 	public Pessoa getPessoa() {
 		return pessoa;
 	}
 
 	public void setPessoa(Pessoa pessoa) {
 		this.pessoa = pessoa;
+	}
+
+	public ContaPagar getContaPagar() {
+		return contaPagar;
+	}
+
+	public void setContaPagar(ContaPagar contaPagar) {
+		this.contaPagar = contaPagar;
 	}
 
 	@Override
@@ -143,7 +142,7 @@ public class ContaPagar implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		ContaPagar other = (ContaPagar) obj;
+		NotaFiscalCompra other = (NotaFiscalCompra) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
---
+++ src/main/java/lojavirtual/model/Produto.java
@@ -0,0 +1,192 @@
+package lojavirtual.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "produto")
+@SequenceGenerator(name = "seq_produto", sequenceName = "seq_produto", allocationSize = 1, initialValue = 1)
+public class Produto implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_produto")
+	private Long id;
+
+	private String tipoUnidade;
+	private String nome;
+
+	private Boolean ativo = Boolean.TRUE;
+
+	@Column(columnDefinition = "text", length = 2000) // tipo: text, grandona
+	private String descricao;
+
+	/** Nota item produto - Associar **/
+
+	// double as vezes n cria campo no db, Double cria
+	private Double peso; // 1000.55g
+	private Double largura;
+	private Double altura;
+	private Double profundidade;
+	private BigDecimal valorVenda = BigDecimal.ZERO;
+	private Integer qtdEstoque = 0;
+	private Integer qtdAlertaEstoque = 0;
+	private String linkYoutube;
+	private Boolean alertaQtdEstoque = Boolean.FALSE;
+	private Integer qtdClique = 0;
+
+	public void setAtivo(Boolean ativo) {
+		this.ativo = ativo;
+	}
+
+	public Boolean getAtivo() {
+		return ativo;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTipoUnidade() {
+		return tipoUnidade;
+	}
+
+	public void setTipoUnidade(String tipoUnidade) {
+		this.tipoUnidade = tipoUnidade;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Double getPeso() {
+		return peso;
+	}
+
+	public void setPeso(Double peso) {
+		this.peso = peso;
+	}
+
+	public Double getLargura() {
+		return largura;
+	}
+
+	public void setLargura(Double largura) {
+		this.largura = largura;
+	}
+
+	public Double getAltura() {
+		return altura;
+	}
+
+	public void setAltura(Double altura) {
+		this.altura = altura;
+	}
+
+	public Double getProfundidade() {
+		return profundidade;
+	}
+
+	public void setProfundidade(Double profundidade) {
+		this.profundidade = profundidade;
+	}
+
+	public BigDecimal getValorVenda() {
+		return valorVenda;
+	}
+
+	public void setValorVenda(BigDecimal valorVenda) {
+		this.valorVenda = valorVenda;
+	}
+
+	public Integer getQtdEstoque() {
+		return qtdEstoque;
+	}
+
+	public void setQtdEstoque(Integer qtdEstoque) {
+		this.qtdEstoque = qtdEstoque;
+	}
+
+	public Integer getQtdAlertaEstoque() {
+		return qtdAlertaEstoque;
+	}
+
+	public void setQtdAlertaEstoque(Integer qtdAlertaEstoque) {
+		this.qtdAlertaEstoque = qtdAlertaEstoque;
+	}
+
+	public String getLinkYoutube() {
+		return linkYoutube;
+	}
+
+	public void setLinkYoutube(String linkYoutube) {
+		this.linkYoutube = linkYoutube;
+	}
+
+	public Boolean getAlertaQtdEstoque() {
+		return alertaQtdEstoque;
+	}
+
+	public void setAlertaQtdEstoque(Boolean alertaQtdEstoque) {
+		this.alertaQtdEstoque = alertaQtdEstoque;
+	}
+
+	public Integer getQtdClique() {
+		return qtdClique;
+	}
+
+	public void setQtdClique(Integer qtdClique) {
+		this.qtdClique = qtdClique;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Produto other = (Produto) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+}
